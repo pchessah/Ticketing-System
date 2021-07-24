3161752d15034cb42df487d533939cec9e90edc2
Paul Chesa <paulchesa1@gmail.com>
2021-07-24 18:36:33 +0300

    Admin dashboard statistics

--- src/app/admin/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/pages/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ITickets } from 'src/app/shared/libs/interfaces/itickets';
+import { TicketsService } from 'src/app/shared/libs/services/tickets.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,11 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
   public chartType: string = 'bar';
-  public chartDatasets: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'New Tickets' }
-  ];
+ 
 
-  public chartLabels: Array<any> = [ 'New', 'Waiting on us', 'Waiting on contact', 'Completed'];
+  public chartLabels: Array<any> = ['New', 'Waiting on us', 'Waiting on contact', 'Completed'];
 
   public chartColors: Array<any> = [
     {
@@ -38,12 +38,47 @@ export class AdminDashboardComponent implements OnInit {
   };
   public chartClicked(e: any): void { }
   public chartHovered(e: any): void { }
+  allTickets: any[] = [];
+  new!: number
+  waitingOnContact!: number
+  waitingOnUs!: number
+  closed!: number
 
-  constructor() { }
+  public chartDatasets: Array<any> = [  { data: [this.new,this.waitingOnContact,this.waitingOnUs, this.closed], label: 'No. of tickets' } ];
+
+
+  constructor(private ticketService: TicketsService) { }
 
   ngOnInit(): void {
+    this.getTicketNumbers()
   }
 
+  getTicketNumbers() {
+    this.ticketService.getTickets().subscribe(allTickets => {
+      this.allTickets = allTickets.map(e => {
+        const data = e.payload.doc.data() as ITickets
+        return {
+          id: e.payload.doc.id,
+          my_id: data.my_id,
+          category: data.category,
+          contact: data.contact,
+          description: data.description,
+          contact_email: data.contact_email,
+          ticket_status: data.ticket_status
+        }
+      })
+      
+      this.new = this.allTickets.filter((ticket) => ticket.ticket_status == "New").length
+      this.waitingOnContact = this.allTickets.filter((ticket) => ticket.ticket_status =="Waiting on contact").length
+      this.waitingOnUs = this.allTickets.filter((ticket) => ticket.ticket_status == "Waiting on us").length
+      this.closed = this.allTickets.filter((ticket) => ticket.ticket_status =="Closed").length
+
+      
+
+      this.chartDatasets = [  { data: [this.new,this.waitingOnContact,this.waitingOnUs, this.closed], label: 'New Tickets' } ]
+
+    })
+  }
 
 
 }
